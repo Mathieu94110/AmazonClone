95c2d4a668aaf022a3e752035c844507d7752c86
mathieu enault <me94110@gmail.com>
2023-08-08 22:54:51 +0200

    progress on google auth request

--- amazon-clone/App.tsx
+++ amazon-clone/App.tsx
@@ -1,10 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState<any>(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({});
+
+  useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response]);
+
+  function callGoogleAuth() {
+    promptAsync();
+  }
+
+  async function handleSignInWithGoogle() {
+    const user = await AsyncStorage.getItem("@user");
+    if (!user) {
+      if (response?.type === "success") {
+        await getUserInfo(response.authentication?.accessToken);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+
+  async function getUserInfo(token: any) {
+    if (!token) return;
+    try {
+      const response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{JSON.stringify(userInfo)}</Text>
+      <Button title="Se connecter avec Google" onPress={callGoogleAuth} />
+      <Button title="Se déconnecter" onPress={() => AsyncStorage.removeItem("@user")} />
       <StatusBar style="auto" />
     </View>
   );
